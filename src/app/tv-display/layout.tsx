f271c051a21e62440b48218c079390de9c5558a2
Naftanael Ricarte <naftanaell@gmail.com>
2025-06-12 03:30:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/tv-display/layout.tsx
+++
@@ -1,5 +0,0 @@
-// This file's functionality has been moved to src/app/(tv)/tv-display/layout.tsx
-// This content is to prevent Next.js from treating it as a layout for the /tv-display route.
-// Ideally, this file should be deleted from the project.
-
-// No default export is provided to prevent this from being treated as a layout.
