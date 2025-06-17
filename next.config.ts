2453ae5b0e078a6d5d6a5ba3c1d04f88df8119ea
Naftanael Ricarte <naftanaell@gmail.com>
2025-06-17 18:05:43 +0000

    Veriique o erro que ta impedindo a publicação

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false, // Changed from true to false
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false, // Changed from true to false
   },
   images: {
     remotePatterns: [
